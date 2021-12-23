ad768b0e6736ef238d2cbc2520c9cd9e5156249d
a1000rhan <1000rhan@gmail.com>
2021-12-23 09:33:48 +0300

    array

---
+++ index.js
@@ -0,0 +1,13 @@
+let arr=[]
+let hobbies=["running","photography", "videogrphy","swimming","coding","singing"]
+let numbers=[1,3,5,6,7,]
+console.log(numbers[1]);
+console.log(hobbies[5]);
+hobbies.push("football");
+console.log(hobbies[6]);
+console.log(hobbies.length);
+hobbies.pop();
+hobbies.pop();
+console.log(hobbies.length);
+hobbies =[];
+console.log(hobbies);
